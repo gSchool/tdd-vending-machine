e3aad45f9a0c391e3db2aa53a20aadedd317cd4f
rachel.koldenhoven <rachel@koldenfrozen.com>
2018-08-05 11:16:42 +0530

    seeSelections

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,15 @@
 module.exports = class Machine {
     constructor() {
+        this.inventory = [
+            {'crisps': 100},
+            {'chocolate': 350},
+            {'mints': 70}
+        ];
 
     }
 
 
     seeSelections() {
+        return this.inventory;
     }
 };
