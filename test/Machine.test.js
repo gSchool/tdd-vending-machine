78eb19ffd6f541f1b9771aafc6467fb8ce684e7d
Jeffrey Klier <jeffrey.klier@galvanize.com>
2019-04-18 13:08:55 -0400

    refactored base exercise to use jest instead

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,8 +1,7 @@
 const Machine = require('../src/Machine');
-const expect = require('chai').expect;
 
 describe('the vending machine', () => {
-    it('should have items to purchase', () => {
+    test('should have items to purchase', () => {
         // setup
         const machine = new Machine();
         const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
@@ -11,7 +10,7 @@ describe('the vending machine', () => {
         const actual = machine.seeSelections();
 
         // assert
-        expect(actual).to.deep.equal(expected);
+        expect(actual).toEqual(expected);
     });
 
 });
